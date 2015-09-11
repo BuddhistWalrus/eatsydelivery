b56208d74b8701c2bb24385f2dcc8c41e91b045b
BuddhistWalrus <Gabe>
2015-09-10 20:55:10 -0400

    add order submit and database creation

---
+++ app/controllers/order_controller.rb
@@ -0,0 +1,26 @@
+class OrderController < ApplicationController
+
+	skip_before_action :verify_authenticity_token
+
+	def index
+	end
+
+	def create
+		@order = Order.new
+
+		@order.name = params[:name]
+		@order.email = params[:email]
+		@order.number = params[:phone]
+
+		if @order.save
+			redirect_to welcome_complete_path
+		end
+	end
+
+	def new
+	end
+
+	def complete
+	end
+
+end
